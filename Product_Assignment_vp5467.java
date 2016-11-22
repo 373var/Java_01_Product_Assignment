694d1bcb75d1bd8e769fc51b265475bc472458dd
373var <373var@gmail.com>
2016-11-22 14:59:08 -0600

    completed assignment locally

---
+++ Product_Assignment_vp5467.java
@@ -0,0 +1,9 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_<<eid>>.java   (Product_jr007.java)
+// It should have a private sku instance variable containing your eid.
+
+
+class Product_Assignment_vp5467 {
+	private String eid = "vp5467";
+}
